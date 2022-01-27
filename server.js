7429cd2c99c424a7bada91dfa7679b89964ec95b
Mario Lazzari <mario.lazzari@italcoeng.com>
2022-01-27 14:53:44 +0100

    Refactored logs

--- server.js
+++ server.js
@@ -1,49 +1,50 @@
 const express = require("express");
-const os = require("os");
 const cluster = require("cluster");
+const os = require("os");
 const fs = require("fs");
 
+// system info
+const { pid } = process;
+const cores = os.cpus().length;
+
+// express settings
 const app = express();
 app.use(express.json());
 
+// sleep function
 const sleep = ms => {
   const start = Date.now();
   while (new Date() - start < ms) {}
   //return new Promise(resolve => setTimeout(() => resolve(ms), ms));
 };
 
+// main route
 app.get("/", (req, res) => {
-  const { pid } = process;
-  res.status(200).json({ message: "Hello World", pid });
+  const { isMaster } = cluster;
+  res.status(200).json({ pid, isMaster });
 });
 
+// timer route
 app.get("/timer", async (req, res) => {
-  const { pid } = process;
-
-  const delay = await sleep(10000);
-
+  const delay = sleep(10000);
   res.status(200).json({ message: "Timer end", delay, pid });
 });
 
+// fork workers
 if (cluster.isMaster) {
-  console.log("Master process started:", process.pid);
-  const cores = os.cpus().length;
+  console.log("Primary s running", pid);
+
   for (let i = 0; i < cores; i++) {
     cluster.fork();
   }
 
+  // jobs
+  const message = `Hello World from ${pid} at ${new Date().toLocaleTimeString()}\n`;
   setInterval(() => {
-    fs.appendFileSync(
-      "log.txt",
-      "Hello World from: " +
-        process.pid +
-        " at " +
-        new Date().toLocaleTimeString() +
-        "\n"
-    );
+    fs.appendFileSync("log.txt", message);
   }, 1000);
 } else {
   app.listen(3000, () => {
-    console.log("Server is running on port 3000", process.pid);
+    console.log("Server is running on port 3000", pid);
   });
 }
